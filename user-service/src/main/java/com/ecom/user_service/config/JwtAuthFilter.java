cb8e130dde20444b08f7280ac34cef5820ed5680
sauummm <126983204+sauummm@users.noreply.github.com>
2025-09-07 00:53:50 +0530

    cart-service

--- user-service/src/main/java/com/ecom/user_service/config/JwtAuthFilter.java
+++ user-service/src/main/java/com/ecom/user_service/config/JwtAuthFilter.java
@@ -11,12 +11,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthFilter {
+public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
